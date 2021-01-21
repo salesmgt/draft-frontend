0b5794e891457bd925009e8ec6d80f6192caddb9
hoanggia2412 <gianhse63203@fpt.edu.vn>
2021-01-22 06:15:00 +0700

    va loi My tab component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,10 @@
-import React,{useState} from 'react'
+import React from 'react'
 import './App.css'
-import { MyTableTab } from '../MyTableTab'
-import * as Const from '../MyTableTab/MyTableTabConst'
-import {data} from "../../store"
+import {SchoolPage} from "../../pages/SchoolPage"
 function App() {
-  const [schoolTypes,setSchoolTypes] = useState([Const.PRIMARY_SCHOOL,Const.SECONDARY_SCHOOL,Const.HIGH_SCHOOL]);
-  const [category,setCategory] = useState(Const.PRIMARY_SCHOOL)
-  const handleCategorySelected = category => {
-    setCategory(category)
-    const newSchool = schools.filter( item => item.type === category)
-  }
-  return <MyTableTab schoolTypes={schoolTypes}  category={category}
-  onSelect={handleCategorySelected} />
+  
+  return <SchoolPage/>
+  
 }
 
 export default App
--- src/pages/SchoolPage/SchoolPage.js
+++ src/pages/SchoolPage/SchoolPage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import PropTypes from 'prop-types'
-
+import { MyTableTab } from '../../components/MyTableTab'
+import * as Const from '../../components/MyTableTab/MyTableTabConst'
+import {data} from "../../store"
 
 function SchoolPage(props) {
-  return <></>
+  const [schoolTypes,setSchoolTypes] = useState([Const.PRIMARY_SCHOOL,Const.SECONDARY_SCHOOL,Const.HIGH_SCHOOL]);
+  const [category,setCategory] = useState(Const.PRIMARY_SCHOOL)
+  const handleCategorySelected = category => {
+    setCategory(category)
+
+    //array is filtered
+    const newSchool = data.filter( item => item.level === category)
+    console.log(newSchool)
+  }
+  return <MyTableTab schoolTypes={schoolTypes}  category={category}
+  onSelect={handleCategorySelected}/>
 }
+
 
 SchoolPage.propTypes = {}
 
